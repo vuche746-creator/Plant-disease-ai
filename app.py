9fba955e97cc62e223f21c1e2e4c42bb118e8aaf
vuche746-creator <vuche746@gmail.com>
2026-06-22 12:58:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ model = tf.keras.models.load_model("plant_disease_final.h5")
 # Load classes
 with open("class_names.json", "r") as f:
     class_names = json.load(f)
-
+with open("disease_info.json", "r") as f:
+    disease_info = json.load(f)
 st.title("🌱 Plant Disease Detection AI")
 
 uploaded_file = st.file_uploader(
@@ -49,5 +50,52 @@ if uploaded_file is not None:
 
     disease = class_names[predicted_index]
 
-    st.success(f"Disease: {disease}")
-    st.info(f"Confidence: {confidence*100:.2f}%")
+    #st.success(f"Disease: {disease}")
+    #st.info(f"Confidence: {confidence*100:.2f}%")
+predicted_class = class_names[predicted_index]
+
+confidence = float(np.max(predictions)) * 100
+
+parts = predicted_class.split("___")
+
+plant_name = parts[0].replace("_", " ")
+
+if len(parts) > 1:
+    disease_name = parts[1].replace("_", " ")
+else:
+    disease_name = "Unknown"
+
+info = disease_info.get(predicted_class, {})
+
+description = info.get(
+    "description",
+    "No description available."
+)
+
+treatment = info.get(
+    "treatment",
+    ["No treatment information available."]
+)
+
+st.success("Prediction Complete")
+
+st.subheader("🌱 Plant")
+st.write(plant_name)
+
+if "healthy" in predicted_class.lower():
+    st.subheader("✅ Status")
+    st.write("Healthy")
+else:
+    st.subheader("🦠 Disease")
+    st.write(disease_name)
+
+st.subheader("📊 Confidence")
+st.write(f"{confidence:.2f}%")
+
+st.subheader("📖 Description")
+st.write(description)
+
+st.subheader("💊 Recommended Treatment")
+
+for item in treatment:
+    st.write(f"• {item}")
